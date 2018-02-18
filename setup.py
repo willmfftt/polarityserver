e9336ea534ce0d03d89466ea629321bb80d852ba
William Moffitt <wmoffitt@cybrtalk.com>
2018-02-18 14:49:14 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='polarityserv',
+    version='0.1dev',
+    packages=[
+    ],
+    license='',
+    author='William Moffitt',
+    author_email='wmoffitt@cybrtalk.com',
+    description='',
+    entry_points={
+        'console_scripts': [
+            'polarityserv = polarity_server.__main__:main',
+        ],
+    },
+    install_requires=[
+    ],
+    python_requires='~=3.6',
+)
